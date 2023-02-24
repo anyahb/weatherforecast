94341f0b85a906d2ba77ea30588bb6e76bf60a68
Анна Артемьева <artemiden@Annas-MacBook-Pro.local>
2023-02-24 01:33:54 -0500

    network problems

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
 
     const getWeather = () => {
-        fetch("http://api.weatherapi.com/v1/forecast.json?key=90bcae4386e744f082d174730222407&q=Washington DC&days")
+        fetch("https://api.weatherapi.com/v1/forecast.json?key=90bcae4386e744f082d174730222407&q=Washington DC&days")
             .then((response) => {
                 return response.json()
             })
@@ -23,7 +23,6 @@ function App() {
                 setForecastDays(res.forecast.forecastday)
 
                 getTime(res.forecast.forecastday)
-
 
                 const current = res.current
                 const finalIcon = current.condition.icon
@@ -36,13 +35,12 @@ function App() {
 
 
     const getAllDays = () => {
-        fetch("http://api.weatherapi.com/v1/forecast.json?key=90bcae4386e744f082d174730222407&q=Washington DC&days=3")
+        fetch("https://api.weatherapi.com/v1/forecast.json?key=90bcae4386e744f082d174730222407&q=Washington DC&days=3")
             .then((response) => {
                 return response.json()
             })
 
             .then((res) => {
-                console.log("checkforsure", res.forecast.forecastday)
                 setAllDays(res.forecast.forecastday)
             })
     }
@@ -58,13 +56,16 @@ function App() {
 
 
     const dateFormat = (el) => {
-        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-        const sorter = el.split("-")
-        const hey = sorter[1].slice(1)
-        const resultMonths = months[parseInt(hey) - 1]
-        const num = sorter[2] * 1
-        const year = sorter[0]
-        return `${num} ` + `${resultMonths} ` + `${year}`
+        if (el) {
+            const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+            const sorter = el.split("-")
+            const hey = sorter[1].slice(1)
+            const resultMonths = months[parseInt(hey) - 1]
+            const num = sorter[2] * 1
+            const year = sorter[0]
+            return `${num} ` + `${resultMonths} ` + `${year}`
+        }
+            return ""
     }
 
 
@@ -73,14 +74,12 @@ function App() {
     let day = weekday[d.getDay()];
     let time = new Date().toLocaleTimeString([], {hour: '2-digit', minute: "2-digit"})
 
-    // const currentHour = new Date().getHours()
 
-    console.log("fordays", forecastDays)
+
     const currentDay = forecastDays[0]
-    console.log("curDay", currentDay)
 
 
-    // const timeArray = currentDay?.hour ?? []
+
 
     let getTime = (el) => {
         const currentHour = new Date().getHours()
@@ -105,6 +104,7 @@ function App() {
 
         const temperature = currentWeather?.temp_f
         setTemperature(temperature)
+        console.log(temperature)
         setResult(laterHours)
     }
 
@@ -118,46 +118,31 @@ function App() {
     return (
         <div>
 
-            {forecastDays.map((item) => {
-                return (
-
                     <div className="forecast">
-
-
                         <div className="forecast__left">
-
-
                             <img className='forecast__icon' src={forecastIcon} alt=""></img>
 
                             <div className='forecast__temperature'>
                                 {temperature}°
                             </div>
-
-
                             <div className="forecast__additional">
-                                <div className="forecast__precipitation">Precipitation: {item.hour[0].precip_in}%</div>
-                                <div className="forecast__humidity">Humidity: {item.hour[0].humidity}%</div>
+                                <div className="forecast__precipitation">Precipitation: {currentDay?.hour[0]?.precip_in}%</div>
+                                <div className="forecast__humidity">Humidity: {currentDay?.hour[0]?.humidity}%</div>
                                 <div className="forecast__wind">
                                     <div>
-                                        Wind: {item.hour[0].wind_mph} mph
+                                        Wind: {currentDay?.hour[0]?.wind_mph} mph
                                     </div>
                                 </div>
                                 {/*  <div className="forecast__temp">Temperature Now: {temperature}°</div>*/}
                             </div>
-
                         </div>
-
                         <div className="forecast__right">
                             <div className="forecast__title">Hourly Weather</div>
                             <div className="forecast__city">{forecastLocation}</div>
-                            <div className="forecast__weekday">{day}, {dateFormat(item.date)}</div>
+                            <div className="forecast__weekday">{day}, {dateFormat(currentDay?.date)}</div>
                             <div className="forecast__time">{time}</div>
-
                         </div>
                     </div>
-
-                )
-            })}
 
 
             <div className='container'>
@@ -181,7 +166,7 @@ function App() {
                                                 </div>
 
 
-                                                <div class="forecast-day__output">
+                                                <div className="forecast-day__output">
 
                                                     <div className="forecast-day__temperature">{elem.temp_f}°</div>
 
@@ -232,7 +217,7 @@ function App() {
                         return (
 
 
-                            <div class="secondDay__temp">
+                            <div className="secondDay__temp">
 
                                 <div className="secondDay__date">{day}, {dateFormat(item.date)}</div>
 
@@ -265,7 +250,6 @@ function App() {
                                                     <div className="secondDay__wind">{item.wind_mph} mph</div>
 
                                                 </div>
-
 
                                             </div>
                                         )
